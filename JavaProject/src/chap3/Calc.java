3c26a938dd26d4cc53873693fe8655addcbed647
rissins <tnalsqqq@naver.com>
2021-07-28 17:26:34 +0900

    Array

---
+++ JavaProject/src/chap3/Calc.java
@@ -0,0 +1,13 @@
+package chap3;
+
+public class Calc {
+	public static void main(String[] args) {
+		double sum = 0.0;
+		
+		for (int i = 0; i < args.length; i++) {
+			sum += Double.parseDouble(args[i]);
+		}
+		
+		System.out.println("гу╟Х"+ sum);
+	}
+}
