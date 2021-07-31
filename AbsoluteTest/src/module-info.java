8825cda8de6f87169dd36ddc7aafb44acc87c4d2
rissins <tnalsqqq@naver.com>
2021-07-31 21:45:38 +0900

    ReView

---
+++ AbsoluteTest/src/module-info.java
@@ -0,0 +1,22 @@
+module TDD {
+	exports aa;
+	exports app;
+	exports Bank;
+	exports chap4;
+	exports CarGame3;
+	exports CheckBoxTest;
+	exports main;
+	exports Array;
+	exports Project;
+	exports etc;
+	exports Example;
+	exports MovieTest;
+	exports chap3;
+	exports Trans;
+	exports derived;
+	exports CheckRadio;
+	exports base;
+
+	requires TDD;
+	requires java.desktop;
+}
