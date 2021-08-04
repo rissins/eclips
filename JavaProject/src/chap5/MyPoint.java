d128385cc731ed276137f6a96e343cb377efb55b
rissins <tnalsqqq@naver.com>
2021-08-04 15:34:12 +0900

    toString, Constructer Ex

---
+++ JavaProject/src/chap5/MyPoint.java
@@ -0,0 +1,26 @@
+package chap5;
+
+
+public class MyPoint {
+	private int x, y;
+	public MyPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "Point("+x+","+y+")";
+	}
+	public static void main(String[] args) {
+		
+		MyPoint p = new MyPoint(3, 50);
+		MyPoint q = new MyPoint(4, 50);
+		System.out.println(p);
+		if(p.equals(q)) {
+			System.out.println("같은 점");
+		}else {
+			System.out.println("다른 점");
+		}
+	}
+}
