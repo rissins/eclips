1bf1860c583440816571f05508d1a811ff7eea2d
rissins <tnalsqqq@naver.com>
2021-08-04 15:32:19 +0900

    SuperClass Example

---
+++ JavaProject/src/chap5/SuperEX.java
@@ -0,0 +1,35 @@
+package chap5;
+
+class Point2{
+	private int x, y;
+	public Point2() {
+		this.x = this.y = 0;
+	}
+	public Point2(int x, int y) {
+		this.x = x; 
+		this.y = y;
+	}
+	public void showPoint() {
+		
+		System.out.println("("+x+","+y+")");
+	}
+}
+
+class ColorPoint2 extends Point2{
+	private String color;
+	public ColorPoint2(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+	public void showColorPoint() {
+		
+		System.out.println(color);
+		showPoint();
+	}
+}
+public class SuperEX {
+	public static void main(String[] args) {
+		ColorPoint2 cP2 = new ColorPoint2(5, 6, "blue");
+		cP2.showColorPoint();
+	}
+}
