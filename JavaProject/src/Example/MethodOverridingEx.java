fb040098af0b181cab41b6a76b990d967c5e13f4
rissins <tnalsqqq@naver.com>
2021-08-06 14:04:43 +0900

    Overriding Ex

---
+++ JavaProject/src/Example/MethodOverridingEx.java
@@ -0,0 +1,43 @@
+package Example;
+
+class Shape{
+	public void draw() {
+		System.out.println("Shape");
+	}
+}
+
+class Line extends Shape{
+	@Override // annotation
+	public void draw() {
+		System.out.println("Line");
+	}
+}
+
+class Rect extends Shape{
+	@Override
+	public void draw() {
+		System.out.println("Rect");
+	}
+}
+
+class Circle extends Shape{
+	@Override
+	public void draw() {
+		System.out.println("Circle");
+	}
+}
+
+public class MethodOverridingEx{
+	static void paint(Shape p) { // Shape p = new Line();
+		p.draw();
+	}
+	
+	public static void main(String[] args) {
+		Line line = new Line();
+		paint(line);
+		paint(new Shape());
+		paint(new Line());
+		paint(new Rect());
+		paint(new Circle());
+	}
+}
