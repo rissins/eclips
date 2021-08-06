dd173df4e33a942c9e77c376f78e09ed3cdf3856
rissins <tnalsqqq@naver.com>
2021-08-06 15:10:51 +0900

    Overriding Ex

---
+++ JavaProject/src/chap5/Overriding.java
@@ -0,0 +1,27 @@
+package chap5;
+
+class Weapon{
+	
+	protected int fire() { // int : 리턴타입, fire : method
+		return 1;
+	}
+}
+
+class Cannon extends Weapon{
+	@Override
+	protected int fire() {
+		return 10;
+	}
+}
+
+public class Overriding {
+	public static void main(String[] args) {
+		Weapon weapon;
+		weapon = new Weapon();
+		System.out.println("기본 무기의살상 느력은"+ weapon.fire());
+		
+		weapon = new Cannon();
+		System.out.println("대포 무기의살상 느력은"+ weapon.fire());
+		
+	}
+}
